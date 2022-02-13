8af4baf9a06ff24ae78c24a48e0e72f805608e4e
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-13 18:18:14 +0530

    13-02-2022 : TurnaryOperators

---
+++ operators/turnaryOperators.js
@@ -0,0 +1,24 @@
+// * PRATICAL APPLICATION 
+
+/*
+  * Ticket Identifier app
+*/
+
+// If person's age is more than 18, They are 'Adult' cateogry, otherwise, They are a 'Child' category
+
+let age = 14;
+
+// * condition ? 'value1' : 'value 2'
+let type = age > 18 ? "Adult Ticket" : "Child Ticket"
+
+console.log(type)
+
+// ! Before turnary Operator
+// if(condition){
+//     // condition runs
+//     let type = 'Adult Ticket';
+// } else {
+
+//     let type = 'Child Ticket'
+//     //  else runs
+// }
