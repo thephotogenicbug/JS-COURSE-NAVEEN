9da9232d286ab292b2fbd46303c65dfd6fc2e071
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-13 18:09:24 +0530

    13-02-2022 : Equality Operators

---
+++ operators/equalityOperator.js
@@ -0,0 +1,8 @@
+// Strict Equality Operator, same (Datatype + Value)
+console.log(1 === 1); // Number === Number
+console.log("1" === 1); // String === Number
+console.log("--------------------------------------")
+// Lose Eqaulity Operator
+console.log(1 == 1); // Number == Number
+console.log("1" == 1); // String == Number , 1st value is important
+console.log(true == "1"); 
