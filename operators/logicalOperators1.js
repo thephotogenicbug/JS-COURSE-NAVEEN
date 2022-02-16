398cf99cc56c1cc9cca6a95f0db39df55c272830
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-16 10:38:32 +0530

    16-02-2022 : uncomment logical AND , Logical OR

--- operators/logicalOperators1.js
+++ operators/logicalOperators1.js
@@ -1,28 +1,28 @@
 // Logical operators
-// *highIncome, CIBILScore -> Load Eligible, Ineligible
+// *highIncome, CIBILScore -> Loan Eligible, Ineligible
 
 
 // Logical AND (&&)
 // returns TRUE if both operands are TRUE
-// let highIncome = true;
-// let CIBILScore = true;
+let highIncome = true;
+let CIBILScore = true;
 
-// let eligiblePerson = highIncome && CIBILScore
+let eligiblePerson = highIncome && CIBILScore
 
-// console.log("Status : " + eligiblePerson);
+console.log("Status : " + eligiblePerson);
 
 
 // Logical OR (||)
 // returns TRUE if any one operands are TRUE
-// let highIncome = true;
-// let CIBILScore = false;
+let highIncome = true;
+let CIBILScore = false;
 
-// let eligiblePerson = highIncome || CIBILScore;
+let eligiblePerson = highIncome || CIBILScore;
 
-// let applicationStatus = eligiblePerson
+let applicationStatus = eligiblePerson
 
-// console.log("Loan Status : " + eligiblePerson);
-// console.log("Application Status : " + applicationStatus);
+console.log("Loan Status : " + eligiblePerson);
+console.log("Application Status : " + applicationStatus);
 
 // NOT (!)
 let highIncome = true;
