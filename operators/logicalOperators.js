beb3079817be275cee429babc8fecc12c6f39ae2
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-16 10:36:58 +0530

    16-02-2022 : logicalOperators

---
+++ operators/logicalOperators.js
@@ -0,0 +1,19 @@
+// Logical operators
+
+
+// Logical AND (&&)
+// returns TRUE if both operands are TRUE
+console.log(true && true);
+// console.log(degree && skill);
+
+console.log("---------------------------------------------------")
+
+// Logical OR (||)
+// returns TRUE if any one operands are TRUE
+console.log(true || false)
+
+console.log("----------------------------------------------------")
+
+// NOT (!)
+console.log(true)
+
