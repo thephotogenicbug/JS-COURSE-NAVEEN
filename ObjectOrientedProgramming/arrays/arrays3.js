73a46837a1c129dc4a3e24717a8eefe7073544a6
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-17 11:40:06 +0530

    17-02-2022 : arrow function

--- ObjectOrientedProgramming/arrays/arrays3.js
+++ ObjectOrientedProgramming/arrays/arrays3.js
@@ -9,8 +9,13 @@ const orders = [
 // console.log(orders.includes({ id: 1, item: "Smartphones", quantity: 1 }));
 
 // callback method , iam gonna call the orders array and find it and function will run the method it will keep on finding one Object at a time
-let result = orders.find(function(order){
-   return order.item === "Laptop" // return if true
-})
+// let result = orders.find(function(order){
+//    return order.item === "Laptop" // return if true
+// })
+
+// Arrow function
+let result = orders.find((order) => {
+  return order.item === "Laptop"; // return if true
+});
 
 console.log(result)
