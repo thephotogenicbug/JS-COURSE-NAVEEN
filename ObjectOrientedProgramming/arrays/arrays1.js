689c5ef359cf96b2e755abc6f8b201bfe51297bc
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-17 11:20:56 +0530

    17-02-2022 : Finding element in javascript array
    primitive-dataTypes

--- ObjectOrientedProgramming/arrays/arrays1.js
+++ ObjectOrientedProgramming/arrays/arrays1.js
@@ -1,28 +1,27 @@
+// Method for finding primitive data-types
+
 // Adding element to javaScript array  // string, index, boolean, undefined, null
-// const cart = ["Fruits", 1, true, undefined, null] 
+// const cart = ["Fruits", 1, true, undefined, null]
 // arrays can contain several values and it can be different data-types
 
 // const number = [4, 5, 6];
 const number = ["Apple", "Lemon", "Grapes"];
 
-
 /* push data inside existing array dynamically, 
 u can modify / edit the value inside the array 
 but you cant remove the array and change it to other data types
-*/ 
+*/
 // normal method
 // number[3] = "Orange"
 
-
-
 // push
-number.push("Guava", "Jackfruit") // it will add the values at the end
+number.push("Guava", "Jackfruit"); // it will add the values at the end
 
 // unshift
 number.unshift("Orange", "Strawberries"); // it will add the values at the start
 
 // splice
-number.splice(3, 0, "Watermelons", "Bananas" )// it will add the values at the middle
+number.splice(3, 0, "Watermelons", "Bananas"); // it will add the values at the middle
 
 // console.log(number)
 // access single value using there index
---
+++ ObjectOrientedProgramming/arrays/arrays2.js
@@ -0,0 +1,24 @@
+// Method for finding primitive data-types
+
+// Finding element in javascript array
+const items = ["Apple", "Lemon", "Grapes"]
+// const items = ["Apple", "Lemon", "Grapes", "Apple"]; //multiple values inside array
+
+// method to find index of an element
+// console.log(items.indexOf("Apple"))
+// console.log(items.lastIndexOf("Apple"));
+// console.log(items.indexOf("apple")); //javaScript is case-sensitive
+
+// Logical method
+// console.log(items.indexOf("Grapes") !== -1); // if the value exist in array show true otherwise false
+
+// you can use includes method to check weather the value is there r not inside the array
+console.log(items.includes("Lemon"));
+
+/*
+  This all coms under primitive-data-types
+  string
+  number 
+  undefined
+  null
+*/
